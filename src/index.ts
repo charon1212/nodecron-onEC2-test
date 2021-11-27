656ee22f6ca90c2ca4dd22bee7923ca025fd61c3
charon1212 <charons2turquoise@gmail.com>
2021-11-27 10:57:39 +0900

    ファイル出力を追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as cron from 'node-cron';
+import * as fs from 'fs';
 
 const main = () => {
   console.log('register')
+  fs.mkdirSync('sample', { recursive: true });
   cron.schedule('*/3 * * * * *', () => {
     console.log('hello')
+    fs.appendFileSync('sample/test.txt', 'newline\n', {});
   })
 };
 
